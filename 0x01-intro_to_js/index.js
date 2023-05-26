364486926b5a751b5fceedbb92b59956ff13f8c0
agyekumdavid36 <134398274+agyekumdavid36@users.noreply.github.com>
2023-05-26 08:11:59 -0700

    Am now learning JS

---
+++ 0x01-intro_to_js/index.js
@@ -0,0 +1,4 @@
+let myName = "David";
+//"when you try to call the variable myName, a popup shows that it is a string. However just the fact that it value is in quotation marks declares it as a string"
+myName = "Agyekum";
+const Car = { type:"Fiat", model:"500", color:"white", fuel:"super", passengers:"5",  height:"3452",weight:"23678",width:"700",axles:"double",safe:True,fast :"True"  } ;
---
+++ 0x01-intro_to_js/numbers.js
@@ -0,0 +1,8 @@
+const favoriteNumber = 12;
+const halfOfFavNum = favoriteNumber/2;
+let isaacsFavorite = 13;
+Result = isaacsFavorite - favoriteNumber;
+isaacsFavorite = 26 * isaacsFavorite;
+isaacsFavorite = isaacsFavorite % 3;
+
+
